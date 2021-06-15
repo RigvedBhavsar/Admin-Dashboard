e867e9f3ec4b2e0a36f786922b61c2ccda4298f2
RigvedBhavsar <rigvedbhavsar@gmail.com>
2021-06-15 13:00:28 +0530

    Display and Update User Details

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Navbar from './components/Navbar/Navbar';
 import Sidenav from './components/Sidenav/Sidenav.jsx';
 import Home from './pages/Home/Home.jsx';
-
+import Userlist from './pages/Userlist/Userlist.jsx';
+import User from "./pages/User/User";
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <div className="container">
-        <Sidenav/>
-        <Home/>
-      </div>
-    </div>
-  );
+	return (
+		<Router>
+			<Navbar />
+			<div className="container">
+				<Sidenav />
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/users">
+						<Userlist />
+					</Route>
+					<Route path="/user/:userId">
+						<User />
+					</Route>
+					{/* <Route path="/newUser">
+						<NewUser />
+					</Route> */}
+					{/* <Route path="/products">
+						<ProductList />
+					</Route> */}
+					{/* <Route path="/product/:productId">
+						<Product />
+					</Route> */}
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
